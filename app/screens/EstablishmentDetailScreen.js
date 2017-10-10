3b53bfc043f6391c4ea1fd68a9d0c9b569b2bb7c
Alejandro Baez <nativo.alejandro@gmail.com>
2017-10-09 22:21:26 -0500

    :lipstick: added transparent style to establishment detail header

--- app/screens/EstablishmentDetailScreen.js
+++ app/screens/EstablishmentDetailScreen.js
@@ -1,35 +1,46 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Card } from 'react-native-elements';
+import Layout from '../constants/Layout';
 
 export default class EstablishmentDetailScreen extends React.Component {
   static navigationOptions = {
-		tabBarVisible: false
-	};
-	
+		tabBarVisible: false,
+		headerStyle: {
+			position: 'absolute',
+			backgroundColor: 'transparent',
+			zIndex: 100,
+			top: 0,
+			left: 0,
+			right: 0,
+			elevation: 0,       //remove shadow on Android
+			shadowOpacity: 0  //remove shadow on iOS
+		}
+  };
+
   render() {
-		const { params } = this.props.navigation.state;
+    const { params } = this.props.navigation.state;
     return (
-      <View style={styles.container}>
-				<Text style={styles.button}>i'm detail screen {`${params.establishmentId}`}</Text>
-      </View>
+      <View>
+				<Image
+					style={{ width: Layout.window.width, height: 192 }} 
+					source={{uri: 'http://lorempixel.com/342/192/food/'}} 
+				/> 
+			</View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#fff',
-	},
-	button: {
-		padding: 10,
-		borderColor: '#000',
-		borderWidth: 1	
-	}
+  },
+  button: {
+    padding: 10,
+    borderColor: '#000',
+    borderWidth: 1,
+  },
 });
